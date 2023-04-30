cd4c6e10714e34c15254be3c12ac6a931488ed79
gs-ssh16 <SaiSriharsha.Annepu@gs.com>
2023-04-30 22:55:01 +0530

    Postgres limit/offset SQL translation support

--- pylegend/tests/extensions/database/vendors/postgres/test_postgres_sql_to_string.py
+++ pylegend/tests/extensions/database/vendors/postgres/test_postgres_sql_to_string.py
@@ -13,8 +13,17 @@
 # limitations under the License.
 
 
-from pylegend.core.databse.sql_to_string import SqlToStringGenerator
 from pylegend.extensions.database.vendors.postgres.postgres_sql_to_string import PostgresSqlToStringGenerator
+from pylegend.core.databse.sql_to_string import (
+    SqlToStringGenerator,
+    SqlToStringConfig,
+    SqlToStringFormat
+)
+from pylegend.core.sql.metamodel import (
+    QuerySpecification,
+    Select,
+    IntegerLiteral
+)
 
 
 class TestPostgresSqlToString:
@@ -22,3 +31,54 @@ class TestPostgresSqlToString:
     def test_postgres_sql_to_string_extension_loaded(self) -> None:
         generator = SqlToStringGenerator.find_sql_to_string_generator_for_db_type("Postgres")
         assert isinstance(generator, PostgresSqlToStringGenerator)
+
+    def test_postgres_process_top(self) -> None:
+        generator = SqlToStringGenerator.find_sql_to_string_generator_for_db_type("Postgres")
+        extension = generator.get_db_extension()
+        config = SqlToStringConfig(SqlToStringFormat(pretty=False))
+
+        query = QuerySpecification(
+            select=Select(selectItems=[], distinct=False), from_=[], where=None, groupBy=[],
+            having=None, orderBy=[], limit=None, offset=None
+        )
+        assert extension.process_top(query, config) == ""
+
+        query.limit = IntegerLiteral(101)
+        assert extension.process_top(query, config) == ""
+
+        query.offset = IntegerLiteral(202)
+        assert extension.process_top(query, config) == ""
+
+    def test_postgres_process_limit(self) -> None:
+        generator = SqlToStringGenerator.find_sql_to_string_generator_for_db_type("Postgres")
+        extension = generator.get_db_extension()
+        config = SqlToStringConfig(SqlToStringFormat(pretty=False))
+
+        query = QuerySpecification(
+            select=Select(selectItems=[], distinct=False), from_=[], where=None, groupBy=[],
+            having=None, orderBy=[], limit=None, offset=None
+        )
+        assert extension.process_limit(query, config) == ""
+
+        query.limit = IntegerLiteral(101)
+        assert extension.process_limit(query, config) == " LIMIT 101"
+
+        query.offset = IntegerLiteral(202)
+        assert extension.process_limit(query, config) == " LIMIT 101 OFFSET 202"
+
+    def test_postgres_process_limit_pretty_format(self) -> None:
+        generator = SqlToStringGenerator.find_sql_to_string_generator_for_db_type("Postgres")
+        extension = generator.get_db_extension()
+        config = SqlToStringConfig(SqlToStringFormat(pretty=True))
+
+        query = QuerySpecification(
+            select=Select(selectItems=[], distinct=False), from_=[], where=None, groupBy=[],
+            having=None, orderBy=[], limit=None, offset=None
+        )
+        assert extension.process_limit(query, config) == ""
+
+        query.limit = IntegerLiteral(101)
+        assert extension.process_limit(query, config) == "\nLIMIT 101"
+
+        query.offset = IntegerLiteral(202)
+        assert extension.process_limit(query, config) == "\nLIMIT 101\nOFFSET 202"
