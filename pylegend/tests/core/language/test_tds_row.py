eb369cc4f818f19be81b2b98ba008749d53fd0e5
gs-ssh16 <SaiSriharsha.Annepu@gs.com>
2023-07-14 14:59:00 +0530

    Number columns

--- pylegend/tests/core/language/test_tds_row.py
+++ pylegend/tests/core/language/test_tds_row.py
@@ -97,6 +97,24 @@ class TestTdsRow:
 
     def test_get_number_col(self) -> None:
         columns = [
+            PrimitiveTdsColumn.number_column("col1"),
+            PrimitiveTdsColumn.string_column("col2")
+        ]
+        frame = LegendApiTableSpecInputFrame(['test_schema', 'test_table'], columns)
+
+        tds_row = TdsRow.from_tds_frame("t", frame)
+        col_expr = tds_row.get_number("col1")
+
+        assert self.db_extension.process_expression(
+            col_expr.to_sql_expression(
+                {"t": frame.to_sql_query_object(self.frame_to_sql_config)},
+                self.frame_to_sql_config
+            ),
+            config=self.sql_to_string_config
+        ) == '"root".col1'
+
+    def test_get_number_col_from_integer(self) -> None:
+        columns = [
             PrimitiveTdsColumn.integer_column("col1"),
             PrimitiveTdsColumn.string_column("col2")
         ]
