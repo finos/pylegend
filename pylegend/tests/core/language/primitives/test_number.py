d6fe733ab4f7867d6c67e11b1e7fedd6cb1fafce
gs-ssh16 <SaiSriharsha.Annepu@gs.com>
2023-07-18 16:40:37 +0530

    Number pos/neg operations

--- pylegend/tests/core/language/primitives/test_number.py
+++ pylegend/tests/core/language/primitives/test_number.py
@@ -93,6 +93,18 @@ class TestPyLegendNumber:
         assert self.__generate_sql_string(lambda x: 1.2 >= x.get_number("col2")) == \
                '("root".col2 <= 1.2)'
 
+    def test_number_pos_expr(self) -> None:
+        assert self.__generate_sql_string(lambda x: + x.get_number("col2")) == \
+               '"root".col2'
+        assert self.__generate_sql_string(lambda x: +(x.get_number("col2") + x.get_number("col1"))) == \
+               '("root".col2 + "root".col1)'
+
+    def test_number_neg_expr(self) -> None:
+        assert self.__generate_sql_string(lambda x: -x.get_number("col2")) == \
+               '(0 - "root".col2)'
+        assert self.__generate_sql_string(lambda x: -(x.get_number("col2") + x.get_number("col1"))) == \
+               '(0 - ("root".col2 + "root".col1))'
+
     def __generate_sql_string(self, f) -> str:  # type: ignore
         return self.db_extension.process_expression(
             f(self.tds_row).to_sql_expression({"t": self.base_query}, self.frame_to_sql_config),
