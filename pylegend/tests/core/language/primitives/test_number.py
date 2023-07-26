64ee2e3f4deebd015df8f919ea452fbfbcc1ed9b
gs-ssh16 <SaiSriharsha.Annepu@gs.com>
2023-07-27 01:06:37 +0530

    Number pow operation

--- pylegend/tests/core/language/primitives/test_number.py
+++ pylegend/tests/core/language/primitives/test_number.py
@@ -119,6 +119,14 @@ class TestPyLegendNumber:
         assert self.__generate_sql_string(lambda x: abs(x.get_number("col2") + x.get_number("col1"))) == \
                'ABS(("root".col2 + "root".col1))'
 
+    def test_number_power_expr(self) -> None:
+        assert self.__generate_sql_string(lambda x: x.get_number("col2") ** x.get_number("col1")) == \
+               'POWER("root".col2, "root".col1)'
+        assert self.__generate_sql_string(lambda x: x.get_number("col2") ** 10) == \
+               'POWER("root".col2, 10)'
+        assert self.__generate_sql_string(lambda x: 1.2 ** x.get_number("col2")) == \
+               'POWER(1.2, "root".col2)'
+
     def __generate_sql_string(self, f) -> str:  # type: ignore
         return self.db_extension.process_expression(
             f(self.tds_row).to_sql_expression({"t": self.base_query}, self.frame_to_sql_config),
