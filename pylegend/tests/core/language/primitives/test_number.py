a0716a158fc599da63073c665bb0b1789b7a27f9
gs-ssh16 <SaiSriharsha.Annepu@gs.com>
2023-07-28 00:18:30 +0530

    Number rem operation

--- pylegend/tests/core/language/primitives/test_number.py
+++ pylegend/tests/core/language/primitives/test_number.py
@@ -172,6 +172,12 @@ class TestPyLegendNumber:
         assert self.__generate_sql_string(lambda x: (x.get_number("col2") + x.get_number("col1")).log()) == \
                'LN(("root".col2 + "root".col1))'
 
+    def test_number_remainder_expr(self) -> None:
+        assert self.__generate_sql_string(lambda x: x.get_number("col2").rem(x.get_number("col1"))) == \
+               'MOD("root".col2, "root".col1)'
+        assert self.__generate_sql_string(lambda x: x.get_number("col2").rem(10)) == \
+               'MOD("root".col2, 10)'
+
     def __generate_sql_string(self, f) -> str:  # type: ignore
         return self.db_extension.process_expression(
             f(self.tds_row).to_sql_expression({"t": self.base_query}, self.frame_to_sql_config),
