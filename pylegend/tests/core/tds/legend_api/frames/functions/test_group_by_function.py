8e772bd7a628eaa9edd8791fc546631177328f5f
gs-ssh16 <SaiSriharsha.Annepu@gs.com>
2023-10-21 19:40:55 +0530

    Integer Min aggregation function

--- pylegend/tests/core/tds/legend_api/frames/functions/test_group_by_function.py
+++ pylegend/tests/core/tds/legend_api/frames/functions/test_group_by_function.py
@@ -410,6 +410,31 @@ class TestGroupByAppliedFunction:
                 "root".col2'''
         assert frame.to_sql_query(FrameToSqlConfig()) == dedent(expected)
 
+    def test_sql_gen_group_by_integer_min_agg(self) -> None:
+        columns = [
+            PrimitiveTdsColumn.integer_column("col1"),
+            PrimitiveTdsColumn.string_column("col2")
+        ]
+        frame: LegendApiTdsFrame = LegendApiTableSpecInputFrame(['test_schema', 'test_table'], columns)
+        frame = frame.group_by(
+            ["col2"],
+            [AggregateSpecification(lambda x: x["col1"], lambda y: y.min(), "Minimum")]  # type: ignore
+        )
+        assert "[" + ", ".join([str(c) for c in frame.columns()]) + "]" == (
+            "[TdsColumn(Name: col2, Type: String), TdsColumn(Name: Minimum, Type: Integer)]"
+        )
+        expected = '''\
+            SELECT
+                "root".col2 AS "col2",
+                MIN(
+                    "root".col1
+                ) AS "Minimum"
+            FROM
+                test_schema.test_table AS "root"
+            GROUP BY
+                "root".col2'''
+        assert frame.to_sql_query(FrameToSqlConfig()) == dedent(expected)
+
     def test_sql_gen_group_by_float_max_agg(self) -> None:
         columns = [
             PrimitiveTdsColumn.float_column("col1"),
@@ -658,6 +683,28 @@ class TestGroupByAppliedFunction:
                              {'values': ['Firm A', 5]},
                              {'values': ['Firm C', 10]},
                              {'values': ['Firm X', 2]}]}
+        res = frame.execute_frame_to_string()
+        assert json.loads(res)["result"] == expected
+
+    def test_e2e_group_by_integer_min_agg(self, legend_test_server: PyLegendDict[str, PyLegendUnion[int, ]]) -> None:
+        frame: LegendApiTdsFrame = simple_trade_service_frame(legend_test_server['engine_port'])
+        frame = frame.group_by(
+            ["Product/Name"],
+            [
+                AggregateSpecification(
+                    lambda x: x['Id'],
+                    lambda y: y.min(),  # type: ignore
+                    'Min Trade Id'
+                )
+            ]
+        )
+        assert "[" + ", ".join([str(c) for c in frame.columns()]) + "]" == \
+               "[TdsColumn(Name: Product/Name, Type: String), TdsColumn(Name: Min Trade Id, Type: Integer)]"
+        expected = {'columns': ['Product/Name', 'Min Trade Id'],
+                    'rows': [{'values': [None, 11]},
+                             {'values': ['Firm A', 3]},
+                             {'values': ['Firm C', 6]},
+                             {'values': ['Firm X', 1]}]}
         res = frame.execute_frame_to_string()
         assert json.loads(res)["result"] == expected
 
