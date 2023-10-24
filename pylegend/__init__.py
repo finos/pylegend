6dae298f0c5ff03accfbb0886ef7c31d893cba86
gs-ssh16 <SaiSriharsha.Annepu@gs.com>
2023-10-24 17:03:29 +0530

    Add release workflow

--- pylegend/__init__.py
+++ pylegend/__init__.py
@@ -11,3 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from pylegend._typing import (
+    PyLegendSequence,
+)
+
+
+__all__: PyLegendSequence[str] = [
+    "__version__"
+]
+
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("pylegend")
