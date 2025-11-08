05530f0994b7e1dbae066b788129dde464e95ee4
Vithesh-Reddy <75151280+Vithesh-Reddy@users.noreply.github.com>
2025-11-08 15:35:52 +0530

    Update pandas_api_base_tds_frame.py for mypy

--- pylegend/core/tds/pandas_api/frames/pandas_api_base_tds_frame.py
+++ pylegend/core/tds/pandas_api/frames/pandas_api_base_tds_frame.py
@@ -15,7 +15,6 @@
 from abc import ABCMeta, abstractmethod
 from datetime import date, datetime
 import pandas as pd
-from typing import Any
 from pylegend._typing import (
     PyLegendSequence,
     PyLegendTypeVar,
@@ -79,7 +78,7 @@ class PandasApiBaseTdsFrame(PandasApiTdsFrame, BaseTdsFrame, metaclass=ABCMeta):
 
     def filter(
             self,
-            items: PyLegendOptional[PyLegendUnion[list[Any], PyLegendList[Any]]] = None,
+            items: PyLegendOptional[PyLegendList[str]] = None,
             like: PyLegendOptional[str] = None,
             regex: PyLegendOptional[str] = None,
             axis: PyLegendOptional[PyLegendUnion[str, int, PyLegendInteger]] = None
