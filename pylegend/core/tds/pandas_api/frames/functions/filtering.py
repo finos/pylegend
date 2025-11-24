baa187311b1baa7735e13677d83118438d2ad6e6
Vithesh-Reddy <75151280+Vithesh-Reddy@users.noreply.github.com>
2025-11-24 16:35:03 +0530

    Update filtering.py

--- pylegend/core/tds/pandas_api/frames/functions/filtering.py
+++ pylegend/core/tds/pandas_api/frames/functions/filtering.py
@@ -37,7 +37,7 @@ class PandasApiFilteringFunction(PandasApiAppliedFunction):
 
     @classmethod
     def name(cls) -> str:
-        return "boolean_filter"  # pragma : no cover
+        return "boolean_filter"  # pragma: no cover
 
     def __init__(
             self,
@@ -65,7 +65,7 @@ class PandasApiFilteringFunction(PandasApiAppliedFunction):
                 type_=LogicalBinaryType.AND,
                 left=new_query.where,
                 right=sql_expr
-            )
+            )  # pragma: no cover
         return new_query
 
     def to_pure(self, config: FrameToPureConfig) -> str:
