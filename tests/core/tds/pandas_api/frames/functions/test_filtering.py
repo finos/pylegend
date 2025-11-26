db38e668cca07514925bead2f9713f3bfc906325
Vithesh-Reddy <75151280+Vithesh-Reddy@users.noreply.github.com>
2025-11-26 17:48:29 +0530

    Update test_filtering.py

--- tests/core/tds/pandas_api/frames/functions/test_filtering.py
+++ tests/core/tds/pandas_api/frames/functions/test_filtering.py
@@ -29,6 +29,10 @@ from pylegend.extensions.tds.pandas_api.frames.pandas_api_table_spec_input_frame
 from tests.test_helpers import generate_pure_query_and_compile
 from tests.test_helpers.test_legend_service_frames import simple_person_service_frame_pandas_api
 from pylegend.core.request.legend_client import LegendClient
+from pylegend.core.database.sql_to_string import (
+    SqlToStringConfig,
+    SqlToStringFormat
+)
 
 
 class TestFilteringFunction:
@@ -128,6 +132,68 @@ class TestFilteringFunction:
         with pytest.raises(TypeError) as v2:
             eval("frame[123]")
         assert v2.value.args[0] == "Invalid key type: <class 'int'>. Expected str, list, or boolean expression"
+
+    def test_filtering_function_on_sql_pure(self) -> None:
+        columns = [
+            PrimitiveTdsColumn.integer_column("col1"),
+            PrimitiveTdsColumn.string_column("col2"),
+            PrimitiveTdsColumn.datetime_column("col3"),
+            PrimitiveTdsColumn.strictdate_column("col4")
+        ]
+        frame: PandasApiTdsFrame = PandasApiTableSpecInputFrame(['test_schema', 'test_table'], columns)
+
+        # Columns
+        col = frame['col3']
+        assert [c.get_name() for c in col.columns()] == ['col3']
+
+        # Pure Query
+        pure = col.to_pure_query()
+        assert pure == dedent(
+            '''\
+            #Table(test_schema.test_table)#
+              ->select(~[col3])'''
+        )
+
+        # SQL Query
+        config = FrameToSqlConfig()
+        sql_object = col.to_sql_query_object(config)  # type: ignore
+        sql_to_string_config = SqlToStringConfig(format_=SqlToStringFormat(pretty=config.pretty))
+        sql = config.sql_to_string_generator().generate_sql_string(sql_object, sql_to_string_config)
+        expected = '''SELECT\n    "root".col3 AS "col3"\nFROM\n    test_schema.test_table AS "root"'''
+        assert sql == expected
+
+    def test_filtering_function_on_column_types(self) -> None:
+        columns = [
+            PrimitiveTdsColumn.integer_column("col1"),
+            PrimitiveTdsColumn.string_column("col2"),
+            PrimitiveTdsColumn.datetime_column("col3"),
+            PrimitiveTdsColumn.strictdate_column("col4")
+        ]
+        frame: PandasApiTdsFrame = PandasApiTableSpecInputFrame(['test_schema', 'test_table'], columns)
+
+        newframe = frame[
+            (frame['col3'] > datetime.datetime(2025, 1, 1)) &  # type: ignore
+            (frame['col4'] == datetime.date(2025, 1, 2))
+            ]
+
+        expected = '''\
+                     SELECT
+                         "root".col1 AS "col1",
+                         "root".col2 AS "col2",
+                         "root".col3 AS "col3",
+                         "root".col4 AS "col4"
+                     FROM
+                         test_schema.test_table AS "root"
+                     WHERE
+                         (("root".col3 > CAST('2025-01-01T00:00:00' AS TIMESTAMP)) AND ("root".col4 = CAST('2025-01-02' AS DATE)))'''  # noqa: E501
+        assert newframe.to_sql_query(FrameToSqlConfig()) == dedent(expected)
+        assert generate_pure_query_and_compile(newframe, FrameToPureConfig(), self.legend_client) == dedent(
+            '''\
+            #Table(test_schema.test_table)#
+              ->filter(c|(($c.col3 > %2025-01-01T00:00:00) && ($c.col4 == %2025-01-02)))'''
+        )
+        assert generate_pure_query_and_compile(newframe, FrameToPureConfig(pretty=False), self.legend_client) == \
+               "#Table(test_schema.test_table)#->filter(c|(($c.col3 > %2025-01-01T00:00:00) && ($c.col4 == %2025-01-02)))"
 
     def test_filtering_function_on_str_input(self) -> None:
         columns = [
@@ -315,6 +381,30 @@ class TestFilteringFunction:
         ]
         frame: PandasApiTdsFrame = PandasApiTableSpecInputFrame(['test_schema', 'test_table'], columns)
 
+        # Nested filter
+        newframe = frame[(frame['col1'] > 10)]  # type: ignore
+        newframe = newframe[newframe['col3'] < 5.5]  # type: ignore
+        expected = '''\
+                   SELECT
+                       "root".col1 AS "col1",
+                       "root".col2 AS "col2",
+                       "root".col3 AS "col3",
+                       "root".col4 AS "col4",
+                       "root".col5 AS "col5"
+                   FROM
+                       test_schema.test_table AS "root"
+                   WHERE
+                       (("root".col1 > 10) AND ("root".col3 < 5.5))'''
+        assert newframe.to_sql_query(FrameToSqlConfig()) == dedent(expected)
+        assert generate_pure_query_and_compile(newframe, FrameToPureConfig(), self.legend_client) == dedent(
+            '''\
+            #Table(test_schema.test_table)#
+              ->filter(c|($c.col1 > 10))
+              ->filter(c|($c.col3 < 5.5))'''
+        )
+        assert generate_pure_query_and_compile(newframe, FrameToPureConfig(pretty=False), self.legend_client) == \
+               "#Table(test_schema.test_table)#->filter(c|($c.col1 > 10))->filter(c|($c.col3 < 5.5))"
+
         # Truncate
         newframe = frame.truncate(before=1, after=3)
         newframe = newframe[newframe['col1'] > newframe['col2']]  # type: ignore
@@ -351,7 +441,7 @@ class TestFilteringFunction:
                "#Table(test_schema.test_table)#->slice(1, 4)->filter(c|($c.col1 > $c.col2))"
 
         # Filter
-        newframe = frame[  # type: ignore
+        newframe = frame[
             ~(  # type: ignore
                     (
                             (frame['col1'] == 2) &
@@ -361,7 +451,7 @@ class TestFilteringFunction:
                     (frame['col5'] >= datetime.date(2003, 11, 10))  # type: ignore
             )
         ]
-        newframe = newframe.filter(items=['col1', 'col5'])
+        newframe = newframe.filter(items=['col1', 'col5'])  # type: ignore
         expected = '''\
                    SELECT
                        "root".col1 AS "col1",
