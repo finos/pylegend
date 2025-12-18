5272700b9999d6fc8d658c24fcadaa0ed5a848e9
Vithesh-Reddy <75151280+Vithesh-Reddy@users.noreply.github.com>
2025-12-18 17:15:29 +0530

    Update pandas_api_table_spec_input_frame.py

--- pylegend/extensions/tds/pandas_api/frames/pandas_api_table_spec_input_frame.py
+++ pylegend/extensions/tds/pandas_api/frames/pandas_api_table_spec_input_frame.py
@@ -28,51 +28,57 @@ from pylegend.core.sql.metamodel import (
 from pylegend.core.tds.pandas_api.frames.pandas_api_input_tds_frame import PandasApiNonExecutableInputTdsFrame
 from pylegend.core.tds.tds_column import TdsColumn
 from pylegend.core.tds.tds_frame import FrameToSqlConfig, FrameToPureConfig
+from pylegend.extensions.tds.abstract.table_spec_input_frame import TableSpecInputFrameAbstract
 
 __all__: PyLegendSequence[str] = [
     "PandasApiTableSpecInputFrame"
 ]
 
 
-class PandasApiTableSpecInputFrame(PandasApiNonExecutableInputTdsFrame):
+class PandasApiTableSpecInputFrame(TableSpecInputFrameAbstract, PandasApiNonExecutableInputTdsFrame):
     table: QualifiedName
 
     def __init__(self, table_name_parts: PyLegendList[str], columns: PyLegendSequence[TdsColumn]) -> None:
-        # TableSpecInputFrameAbstract.__init__(self, table_name_parts=table_name_parts)
-        self.table = QualifiedName(table_name_parts)
+        TableSpecInputFrameAbstract.__init__(self, table_name_parts=table_name_parts)
         PandasApiNonExecutableInputTdsFrame.__init__(self, columns=columns)
 
     def __str__(self) -> str:
         return f"PandasApiTableSpecInputFrame({'.'.join(self.table.parts)})"  # pragma: no cover
 
-    def _build_sql_query_object(self, config: FrameToSqlConfig) -> QuerySpecification:
-        db_extension = config.sql_to_string_generator().get_db_extension()
-        root_alias = db_extension.quote_identifier("root")
-        return QuerySpecification(
-            select=Select(
-                selectItems=[
-                    SingleColumn(
-                        alias=db_extension.quote_identifier(x.get_name()),
-                        expression=QualifiedNameReference(name=QualifiedName(parts=[root_alias, x.get_name()]))
+    def to_sql_query_object(self, config: FrameToSqlConfig) -> QuerySpecification:
+        if self._extra_frame is None:
+            db_extension = config.sql_to_string_generator().get_db_extension()
+            root_alias = db_extension.quote_identifier("root")
+            return QuerySpecification(
+                select=Select(
+                    selectItems=[
+                        SingleColumn(
+                            alias=db_extension.quote_identifier(x.get_name()),
+                            expression=QualifiedNameReference(name=QualifiedName(parts=[root_alias, x.get_name()]))
+                        )
+                        for x in self.columns()
+                    ],
+                    distinct=False
+                ),
+                from_=[
+                    AliasedRelation(
+                        relation=Table(name=self.table),
+                        alias=root_alias,
+                        columnNames=[x.get_name() for x in self.columns()]
                     )
-                    for x in self.columns()
                 ],
-                distinct=False
-            ),
-            from_=[
-                AliasedRelation(
-                    relation=Table(name=self.table),
-                    alias=root_alias,
-                    columnNames=[x.get_name() for x in self.columns()]
-                )
-            ],
-            where=None,
-            groupBy=[],
-            having=None,
-            orderBy=[],
-            limit=None,
-            offset=None
-        )
+                where=None,
+                groupBy=[],
+                having=None,
+                orderBy=[],
+                limit=None,
+                offset=None
+            )
+        else:
+            return self._extra_frame.to_sql_query_object(config)
 
-    def _build_to_pure(self, config: FrameToPureConfig) -> str:
-        return f"#Table({'.'.join(self.table.parts)})#"
+    def to_pure(self, config: FrameToPureConfig) -> str:
+        if self._extra_frame is None:
+            return f"#Table({'.'.join(self.table.parts)})#"
+        else:
+            return self._extra_frame.to_pure(config)
