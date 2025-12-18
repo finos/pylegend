7126cb3305667312a1eaee40d8c4c2cca9be6205
Vithesh-Reddy <75151280+Vithesh-Reddy@users.noreply.github.com>
2025-12-18 17:15:01 +0530

    Update pandas_api_legend_service_input_frame.py

--- pylegend/extensions/tds/pandas_api/frames/pandas_api_legend_service_input_frame.py
+++ pylegend/extensions/tds/pandas_api/frames/pandas_api_legend_service_input_frame.py
@@ -37,16 +37,14 @@ from pylegend.core.tds.tds_frame import (
     FrameToSqlConfig,
     FrameToPureConfig,
 )
+from pylegend.extensions.tds.abstract.legend_service_input_frame import LegendServiceInputFrameAbstract
 
 __all__: PyLegendSequence[str] = [
     "PandasApiLegendServiceInputFrame"
 ]
 
 
-class PandasApiLegendServiceInputFrame(PandasApiExecutableInputTdsFrame):
-    __pattern: str
-    __project_coordinates: ProjectCoordinates
-    __initialized: bool = False
+class PandasApiLegendServiceInputFrame(LegendServiceInputFrameAbstract, PandasApiExecutableInputTdsFrame):
 
     def __init__(
             self,
@@ -54,73 +52,66 @@ class PandasApiLegendServiceInputFrame(PandasApiExecutableInputTdsFrame):
             project_coordinates: ProjectCoordinates,
             legend_client: LegendClient,
     ) -> None:
-        self.__pattern = pattern
-        self.__project_coordinates = project_coordinates
-
-        self._cached_sql = None
-        self._cached_pure = None
-
+        LegendServiceInputFrameAbstract.__init__(self, pattern=pattern, project_coordinates=project_coordinates)
+        self._extra_frame = None
         PandasApiExecutableInputTdsFrame.__init__(
             self,
             legend_client=legend_client,
             columns=legend_client.get_sql_string_schema(self.to_sql_query())
         )
-        self.__initialized = True
-
-    def _build_sql_query_object(self, config: FrameToSqlConfig) -> QuerySpecification:
-        db_extension = config.sql_to_string_generator().get_db_extension()
-        root_alias = db_extension.quote_identifier("root")
-        args: PyLegendList[Expression] = [
-            NamedArgumentExpression(
-                name="pattern",
-                expression=StringLiteral(value=self.__pattern, quoted=False)
-            )
-        ]
-        args += self.__project_coordinates.sql_params()
-        func_call = FunctionCall(
-            name=QualifiedName(["service"]),
-            distinct=False,
-            filter_=None,
-            window=None,
-            arguments=args
-        )
-
-        return QuerySpecification(
-            select=Select(
-                selectItems=[
-                    SingleColumn(
-                        alias=db_extension.quote_identifier(x.get_name()),
-                        expression=QualifiedNameReference(
-                            name=QualifiedName(parts=[root_alias, db_extension.quote_identifier(x.get_name())])
-                        )
-                    )
-                    for x in self.columns()
-                ] if self.__initialized else [AllColumns(prefix=root_alias)],
-                distinct=False
-            ),
-            from_=[
-                AliasedRelation(
-                    relation=TableFunction(functionCall=func_call),
-                    alias=root_alias,
-                    columnNames=[x.get_name() for x in self.columns()] if self.__initialized else []
-                )
-            ],
-            where=None,
-            groupBy=[],
-            having=None,
-            orderBy=[],
-            limit=None,
-            offset=None
-        )
-
-    def to_pure(self, config: FrameToPureConfig) -> str:
-        raise RuntimeError("to_pure is not supported for LegendServiceInputFrame")  # pragma: no cover
-
-    def get_pattern(self) -> str:
-        return self.__pattern  # pragma: no cover
-
-    def set_initialized(self, val: bool) -> None:
-        self.__initialized = val  # pragma: no cover
+        LegendServiceInputFrameAbstract.set_initialized(self, True)
 
     def __str__(self) -> str:
         return f"PandasApiLegendServiceInputFrame({'.'.join(self.get_pattern())})"  # pragma: no cover
+
+    def to_sql_query_object(self, config: FrameToSqlConfig) -> QuerySpecification:
+        if self._extra_frame is None:
+            db_extension = config.sql_to_string_generator().get_db_extension()
+            root_alias = db_extension.quote_identifier("root")
+            args: PyLegendList[Expression] = [
+                NamedArgumentExpression(
+                    name="pattern",
+                    expression=StringLiteral(value=self.get_pattern(), quoted=False)
+                )
+            ]
+            args += self.get_project_coordinates().sql_params()
+            func_call = FunctionCall(
+                name=QualifiedName(["service"]),
+                distinct=False,
+                filter_=None,
+                window=None,
+                arguments=args
+            )
+
+            return QuerySpecification(
+                select=Select(
+                    selectItems=[
+                        SingleColumn(
+                            alias=db_extension.quote_identifier(x.get_name()),
+                            expression=QualifiedNameReference(
+                                name=QualifiedName(parts=[root_alias, db_extension.quote_identifier(x.get_name())])
+                            )
+                        )
+                        for x in self.columns()
+                    ] if self.get_initialized() else [AllColumns(prefix=root_alias)],
+                    distinct=False
+                ),
+                from_=[
+                    AliasedRelation(
+                        relation=TableFunction(functionCall=func_call),
+                        alias=root_alias,
+                        columnNames=[x.get_name() for x in self.columns()] if self.get_initialized() else []
+                    )
+                ],
+                where=None,
+                groupBy=[],
+                having=None,
+                orderBy=[],
+                limit=None,
+                offset=None
+            )
+        else:
+            return self._extra_frame.to_sql_query_object(config)
+
+    def to_pure(self, config: FrameToPureConfig) -> str:
+        raise RuntimeError("to_pure is not supported for LegendServiceInputFrame")  # pragma: no cover
