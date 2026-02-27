1357969dc2fafa3de5e0635b57f0cbe34a746bcd
Shreyansh Singh <92965081+shreyansh2303@users.noreply.github.com>
2026-02-27 16:21:13 +0530

    Lint checks

--- pylegend/core/tds/pandas_api/frames/functions/assign_function.py
+++ pylegend/core/tds/pandas_api/frames/functions/assign_function.py
@@ -39,14 +39,10 @@ from pylegend.core.language.shared.literal_expressions import convert_literal_to
 from pylegend.core.sql.metamodel import (
     QuerySpecification,
     SingleColumn,
-    IntegerLiteral,
 )
 from pylegend.core.tds.pandas_api.frames.functions.rank_function import RankFunction
 from pylegend.core.tds.pandas_api.frames.functions.shift_function import ShiftFunction
-from pylegend.core.tds.pandas_api.frames.helpers.series_helper import (
-    has_window_function,
-    assert_and_find_core_series,
-)
+from pylegend.core.tds.pandas_api.frames.helpers.series_helper import has_window_function
 from pylegend.core.tds.pandas_api.frames.pandas_api_applied_function_tds_frame import PandasApiAppliedFunction
 from pylegend.core.tds.pandas_api.frames.pandas_api_base_tds_frame import PandasApiBaseTdsFrame
 from pylegend.core.tds.sql_query_helpers import copy_query, create_sub_query
