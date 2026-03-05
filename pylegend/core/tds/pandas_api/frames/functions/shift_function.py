4e1cb79496c8a3ebebbabbe9b857fea011041bc4
Shreyansh Singh <92965081+shreyansh2303@users.noreply.github.com>
2026-03-05 15:45:50 +0530

    Pragma: no cover

--- pylegend/core/tds/pandas_api/frames/functions/shift_function.py
+++ pylegend/core/tds/pandas_api/frames/functions/shift_function.py
@@ -160,7 +160,7 @@ class ShiftExtendFunction(PandasApiAppliedFunction):
         return self.__base_frame
 
     def tds_frame_parameters(self) -> PyLegendList["PandasApiBaseTdsFrame"]:
-        return []
+        return []  # pragma: no cover (This frame is never returned directly - it's always followed by shift, diff or pct)
 
     def calculate_columns(self) -> PyLegendSequence["TdsColumn"]:
         temp_column_name_suffix = "__pylegend_olap_column__"
