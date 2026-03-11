2b4f4c6c33c56f530efd6df111fdd93245bbaad4
Vithesh-Reddy <75151280+Vithesh-Reddy@users.noreply.github.com>
2026-03-11 16:53:18 +0530

    Add decimal division and conversion expressions

--- pylegend/core/language/shared/operations/decimal_operation_expressions.py
+++ pylegend/core/language/shared/operations/decimal_operation_expressions.py
@@ -18,6 +18,9 @@ from pylegend._typing import (
 )
 from pylegend.core.language.shared.expression import (
     PyLegendExpressionDecimalReturn,
+    PyLegendExpressionFloatReturn,
+    PyLegendExpressionIntegerReturn,
+    PyLegendExpressionNumberReturn,
 )
 from pylegend.core.language.shared.operations.binary_expression import PyLegendBinaryExpression
 from pylegend.core.language.shared.operations.unary_expression import PyLegendUnaryExpression
@@ -28,9 +31,12 @@ from pylegend.core.sql.metamodel import (
     ArithmeticType,
     ArithmeticExpression,
     NegativeExpression,
+    Cast,
+    ColumnType,
 )
 from pylegend.core.sql.metamodel_extension import (
     AbsoluteExpression,
+    RoundExpression,
 )
 from pylegend.core.tds.tds_frame import FrameToSqlConfig
 from pylegend.core.tds.tds_frame import FrameToPureConfig
@@ -42,6 +48,11 @@ __all__: PyLegendSequence[str] = [
     "PyLegendDecimalNegativeExpression",
     "PyLegendDecimalSubtractExpression",
     "PyLegendDecimalMultiplyExpression",
+    "PyLegendDecimalDivideExpression",
+    "PyLegendDecimalDivideScaledExpression",
+    "PyLegendDecimalRoundExpression",
+    "PyLegendNumberToDecimalExpression",
+    "PyLegendNumberToFloatExpression",
 ]
 
 
@@ -132,6 +143,71 @@ class PyLegendDecimalMultiplyExpression(PyLegendBinaryExpression, PyLegendExpres
         )
 
 
+class PyLegendDecimalDivideExpression(PyLegendBinaryExpression, PyLegendExpressionDecimalReturn):
+
+    @staticmethod
+    def __to_sql_func(
+            expression1: Expression,
+            expression2: Expression,
+            frame_name_to_base_query_map: PyLegendDict[str, QuerySpecification],
+            config: FrameToSqlConfig
+    ) -> Expression:
+        return ArithmeticExpression(ArithmeticType.DIVIDE, expression1, expression2)
+
+    @staticmethod
+    def __to_pure_func(op1_expr: str, op2_expr: str, config: FrameToPureConfig) -> str:
+        return f"({op1_expr} / {op2_expr})"
+
+    def __init__(self, operand1: PyLegendExpressionNumberReturn, operand2: PyLegendExpressionNumberReturn) -> None:
+        PyLegendExpressionDecimalReturn.__init__(self)
+        PyLegendBinaryExpression.__init__(
+            self,
+            operand1,
+            operand2,
+            PyLegendDecimalDivideExpression.__to_sql_func,
+            PyLegendDecimalDivideExpression.__to_pure_func,
+            non_nullable=True,
+            first_operand_needs_to_be_non_nullable=True,
+            second_operand_needs_to_be_non_nullable=True
+        )
+
+
+class PyLegendDecimalDivideScaledExpression(PyLegendExpressionDecimalReturn):
+    __operand1: PyLegendExpressionDecimalReturn
+    __operand2: PyLegendExpressionDecimalReturn
+    __scale: PyLegendExpressionIntegerReturn
+
+    def __init__(
+            self,
+            operand1: PyLegendExpressionDecimalReturn,
+            operand2: PyLegendExpressionDecimalReturn,
+            scale: PyLegendExpressionIntegerReturn,
+    ) -> None:
+        PyLegendExpressionDecimalReturn.__init__(self)
+        self.__operand1 = operand1
+        self.__operand2 = operand2
+        self.__scale = scale
+
+    def to_sql_expression(
+            self,
+            frame_name_to_base_query_map: PyLegendDict[str, QuerySpecification],
+            config: FrameToSqlConfig
+    ) -> Expression:
+        op1 = self.__operand1.to_sql_expression(frame_name_to_base_query_map, config)
+        op2 = self.__operand2.to_sql_expression(frame_name_to_base_query_map, config)
+        scale = self.__scale.to_sql_expression(frame_name_to_base_query_map, config)
+        return RoundExpression(ArithmeticExpression(ArithmeticType.DIVIDE, op1, op2), scale)
+
+    def to_pure_expression(self, config: FrameToPureConfig) -> str:
+        op1 = self.__operand1.to_pure_expression(config)
+        op2 = self.__operand2.to_pure_expression(config)
+        scale = self.__scale.to_pure_expression(config)
+        return generate_pure_functional_call("divide", [op1, op2, scale])
+
+    def is_non_nullable(self) -> bool:
+        return True
+
+
 class PyLegendDecimalAbsoluteExpression(PyLegendUnaryExpression, PyLegendExpressionDecimalReturn):
 
     @staticmethod
@@ -179,6 +255,92 @@ class PyLegendDecimalNegativeExpression(PyLegendUnaryExpression, PyLegendExpress
             operand,
             PyLegendDecimalNegativeExpression.__to_sql_func,
             PyLegendDecimalNegativeExpression.__to_pure_func,
+            non_nullable=True,
+            operand_needs_to_be_non_nullable=True,
+        )
+
+
+class PyLegendDecimalRoundExpression(PyLegendBinaryExpression, PyLegendExpressionDecimalReturn):
+
+    @staticmethod
+    def __to_sql_func(
+            expression1: Expression,
+            expression2: Expression,
+            frame_name_to_base_query_map: PyLegendDict[str, QuerySpecification],
+            config: FrameToSqlConfig
+    ) -> Expression:
+        return RoundExpression(expression1, expression2)
+
+    @staticmethod
+    def __to_pure_func(op1_expr: str, op2_expr: str, config: FrameToPureConfig) -> str:
+        if op2_expr == "0":
+            return generate_pure_functional_call("round", [op1_expr])
+        return generate_pure_functional_call("round", [op1_expr, op2_expr])
+
+    def __init__(self, operand1: PyLegendExpressionDecimalReturn, operand2: PyLegendExpressionIntegerReturn) -> None:
+        PyLegendExpressionDecimalReturn.__init__(self)
+        PyLegendBinaryExpression.__init__(
+            self,
+            operand1,
+            operand2,
+            PyLegendDecimalRoundExpression.__to_sql_func,
+            PyLegendDecimalRoundExpression.__to_pure_func,
+            non_nullable=True,
+            first_operand_needs_to_be_non_nullable=True,
+            second_operand_needs_to_be_non_nullable=True
+        )
+
+    def is_non_nullable(self) -> bool:
+        return True
+
+
+class PyLegendNumberToDecimalExpression(PyLegendUnaryExpression, PyLegendExpressionDecimalReturn):
+
+    @staticmethod
+    def __to_sql_func(
+            expression: Expression,
+            frame_name_to_base_query_map: PyLegendDict[str, QuerySpecification],
+            config: FrameToSqlConfig
+    ) -> Expression:
+        return Cast(expression, ColumnType(name="DECIMAL", parameters=[]))
+
+    @staticmethod
+    def __to_pure_func(op_expr: str, config: FrameToPureConfig) -> str:
+        return generate_pure_functional_call("toDecimal", [op_expr])
+
+    def __init__(self, operand: PyLegendExpressionNumberReturn) -> None:
+        PyLegendExpressionDecimalReturn.__init__(self)
+        PyLegendUnaryExpression.__init__(
+            self,
+            operand,
+            PyLegendNumberToDecimalExpression.__to_sql_func,
+            PyLegendNumberToDecimalExpression.__to_pure_func,
+            non_nullable=True,
+            operand_needs_to_be_non_nullable=True,
+        )
+
+
+class PyLegendNumberToFloatExpression(PyLegendUnaryExpression, PyLegendExpressionFloatReturn):
+
+    @staticmethod
+    def __to_sql_func(
+            expression: Expression,
+            frame_name_to_base_query_map: PyLegendDict[str, QuerySpecification],
+            config: FrameToSqlConfig
+    ) -> Expression:
+        return Cast(expression, ColumnType(name="DOUBLE PRECISION", parameters=[]))
+
+    @staticmethod
+    def __to_pure_func(op_expr: str, config: FrameToPureConfig) -> str:
+        return generate_pure_functional_call("toFloat", [op_expr])
+
+    def __init__(self, operand: PyLegendExpressionNumberReturn) -> None:
+        PyLegendExpressionFloatReturn.__init__(self)
+        PyLegendUnaryExpression.__init__(
+            self,
+            operand,
+            PyLegendNumberToFloatExpression.__to_sql_func,
+            PyLegendNumberToFloatExpression.__to_pure_func,
             non_nullable=True,
             operand_needs_to_be_non_nullable=True,
         )
